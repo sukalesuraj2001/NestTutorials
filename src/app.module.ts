5ebcd183f42524acfe0b6f9b94222345cc01ae73
Suraj <srsukale20@gmail.com>
2024-02-04 19:46:04 +0530

    adding Params decorator, Query decorator and Headers decorators

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users/users.controller';
+import { ParameterController } from './parameter/parameter.controller';
 
 
 @Module({
   imports: [],
-  controllers: [UsersController], //define userController and so on
+  controllers: [UsersController, ParameterController], //define userController and so on
   providers: [],
 })
 export class AppModule {}
